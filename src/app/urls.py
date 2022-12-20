e8683c0852099108997d88f461916971b680dc44
dori-dev <mr.dori.dev@gmail.com>
2022-12-20 15:43:25 +0330

    add group list view

--- src/app/urls.py
+++ src/app/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.UserList.as_view(), name='user_list')
+    path('users/', views.UserList.as_view(), name='user_list'),
+    path('groups/', views.GroupList.as_view(), name='group_list'),
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.views import generic
 
 
@@ -6,3 +6,9 @@ class UserList(generic.ListView):
     model = User
     template_name = 'app/user-list.html'
     context_object_name = 'users'
+
+
+class GroupList(generic.ListView):
+    model = Group
+    template_name = 'app/group-list.html'
+    context_object_name = 'groups'
