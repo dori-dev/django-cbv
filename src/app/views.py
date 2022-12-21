34aa08547420f3559b5ae03a0dffa1ed321b5296
dori-dev <mr.dori.dev@gmail.com>
2022-12-21 17:22:37 +0330

    use View and Template View

--- src/app/views.py
+++ src/app/views.py
@@ -1,5 +1,22 @@
+# from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from django.views import generic
+
+
+# class UserList(generic.View):
+#     def get(self, request, *args, **kwargs):
+#         users = User.objects.all()
+#         context = {
+#             'users': users,
+#         }
+#         return render(request, 'app/user-list.html', context)
+
+
+# class UserList(generic.base.TemplateView):
+#     template_name = 'app/user-list.html'
+#     extra_context = {
+#         'users': User.objects.all(),
+#     }
 
 
 class UserList(generic.ListView):
@@ -12,3 +29,4 @@ class GroupList(generic.ListView):
     model = Group
     template_name = 'app/group-list.html'
     context_object_name = 'groups'
+    paginate_by = 12
