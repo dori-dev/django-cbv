83d6141f8f283ea0c3b18f79a16730ca441a6e0e
dori-dev <mr.dori.dev@gmail.com>
2022-12-22 14:37:55 +0330

    use Create View

--- src/app/urls.py
+++ src/app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('users/', views.UserList.as_view(), name='user_list'),
     path('groups/', views.GroupList.as_view(), name='group_list'),
     path('group/<int:pk>/', views.GroupDetail.as_view(), name='group_detail'),
+    path('create-group/', views.CreateGroup.as_view(), name='create_group'),
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -1,6 +1,8 @@
 # from django.shortcuts import render
+# from django.urls import reverse_lazy
 from django.contrib.auth.models import User, Group
 from django.views import generic
+from django.urls import reverse
 
 
 # class UserList(generic.View):
@@ -27,6 +29,7 @@ class UserList(generic.ListView):
 
 
 class GroupList(generic.ListView):
+    # queryset = Group.objects.order_by('name')
     model = Group
     template_name = 'app/group-list.html'
     context_object_name = 'groups'
@@ -37,3 +40,20 @@ class GroupDetail(generic.DetailView):
     model = Group
     template_name = 'app/group-detail.html'
     # pk_url_kwarg = 'id'
+
+
+class CreateGroup(generic.CreateView):
+    model = Group
+    fields = [
+        'name',
+        'permissions',
+    ]
+    template_name = 'app/create-group.html'
+    # initial = {
+    #     'name': 'group name...'
+    # }
+    # success_url = reverse_lazy('group_list')
+
+    def get_success_url(self) -> str:
+        group: Group = self.object
+        return reverse('group_detail', args=(group.pk,))
